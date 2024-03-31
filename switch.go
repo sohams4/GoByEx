b04074c3bdd0d06bb35f5d85548a93dfe974053e
Soham Shendre <87564877+sohams4@users.noreply.github.com>
2024-03-31 17:17:49 +0530

    Create switch.go

---
+++ switch.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Print("write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's weekday")
+	}
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's Aefore Noon")
+	default:
+		fmt.Println("It's After Noon")
+	}
+	whatami := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I am a bool")
+		case int:
+			fmt.Println("I am int")
+		default:
+			fmt.Printf("Don't know the type %T \n", t)
+		}
+	}
+	whatami(true)
+	whatami(1)
+	whatami("hey")
+}
