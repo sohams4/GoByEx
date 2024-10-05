ef51a5705d94df1dbffcae29e07b4773f17df4d8
Soham Shendre <87564877+sohams4@users.noreply.github.com>
2024-10-05 11:30:09 +0530

    arr.go

--- arr.go
+++ arr.go
@@ -9,7 +9,7 @@ func main() {
      
 	a[4] = 100    
 	fmt.Println("set:", a)   
-	fmt.Println("get:", a[4]) 
+	fmt.Println("get:", a[4])  
  
 	fmt.Println("len:", len(a))
 	b := [5]int{1, 2, 3, 4, 5}
