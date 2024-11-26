d2a14e02b307b6fd53a12fa7215acaa84973482f
Soham Shendre <87564877+sohams4@users.noreply.github.com>
2024-11-26 18:44:43 +0530

    arr.go

--- arr.go
+++ arr.go
@@ -4,7 +4,7 @@ import "fmt"
 func main() {               
 	var a [5]int            
 	fmt.Println("emp:", a)                   
-                                                     
+                                                      
              
 	a[4] = 100      
 	fmt.Println("set:", a)    
