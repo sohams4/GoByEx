154c0eb5f75a09b9ed3b3cd1d970dfbcaceb48a6
Soham Shendre <87564877+sohams4@users.noreply.github.com>
2025-03-09 22:17:52 +0530

    arr.go

--- arr.go
+++ arr.go
@@ -5,7 +5,7 @@ func main() {
 	var a [5]int             
 	fmt.Println("emp:", a)                      
                                                            
-                     
+                      
 	a[4] = 100         
 	fmt.Println("set:", a)        
 	fmt.Println("get:", a[4])    
